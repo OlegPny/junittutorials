783b684d8198f8e9334742e576a6d39f04072291
Oleg Primolennyy <element.onex@gmail.com>
2017-11-19 00:58:39 +0300

    refactor создание вспомогательных обьектов перенесено в метод instantinate

--- controllertesting/src/test/java/TestDefaultController.java
+++ controllertesting/src/test/java/TestDefaultController.java
@@ -6,39 +6,18 @@ import static org.junit.Assert.*;
 public class TestDefaultController {
 
     private DefaultController controller;
+    private RequestHandler handler;
+    private Request request;
 
     //@Before - method is executed just before every of @Test
     //@BeforeClass - method is executed only once before all @Test
     @Before
     public void instantinate() throws Exception {
         controller = new DefaultController();
-    }
-
-    @Test
-    public void testMethod() {
-        //throw new RuntimeException("implement me");
-    }
-
-    @Test
-    public void testAddHandler() {
-        Request request = new SampleRequest();
-        RequestHandler handler = new SampleHandler();
+        handler = new SampleHandler();
+        request = new SampleRequest();
         controller.addHandler(request, handler);
-        RequestHandler handler2 = controller.getHandler(request);
-        assertSame("Handler we set in controller should be the same handler we get",
-                handler, handler2);
     }
-
-    @Test
-    public void testProcessRequest() {
-        Request request = new SampleRequest();
-        RequestHandler handler = new SampleHandler();
-        controller.addHandler(request, handler);
-        Response response = controller.processRequest(request);
-        assertNotNull("Must not return null", response);
-        assertEquals("Response must be of type SampleResponse", SampleResponse.class, response.getClass());
-    }
-
 
     //Classes to use in test (pre-test state or "test fixture")
     private class SampleRequest implements Request {
@@ -58,4 +37,19 @@ public class TestDefaultController {
     private class SampleResponse implements Response {
 
     }
+
+    @Test
+    public void testAddHandler() {
+        RequestHandler handler2 = controller.getHandler(request);
+        assertSame("Handler we set in controller should be the same handler we get",
+                handler, handler2);
+    }
+
+    @Test
+    public void testProcessRequest() {
+        Response response = controller.processRequest(request);
+        assertNotNull("Must not return null", response);
+        assertEquals("Response must be of type SampleResponse", SampleResponse.class, response.getClass());
+    }
+
 }
